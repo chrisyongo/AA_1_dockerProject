b1bfaf1b0bc04548ab005fab77896c8afbdc8655
chrisyongo <christopheryongo@gmail.com>
2023-03-31 09:02:53 +0300

    updated

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,4 @@ FROM php:8.0-apache
 
 COPY . /var/www/html/
 WORKDIR /var/www/html/
-
-COPY signup.php signup.php
-COPY . src
-EXPOSE 8000:80
+EXPOSE 80
--- signup-valid.php
+++ signup-valid.php
@@ -10,14 +10,16 @@
             if(isset($_POST['submit'])){
                 $userName = $_POST['name'];
                 $password = $_POST['password'];
+                $folder_path = 'signup.php';
 
                 if ($userName !== 'johndoe' && $password !== '12345'){
 
                     print('Invalid username or password');
-                    header("Location: signup.php");
+                    echo '<p><a href="' . $folder_path . '">Log in</a></p>';
                     
                 }else{
                     print('Welcome, This is admin!');
+                    echo '<p><a href="' . $folder_path . '">Log out</a></p>';
                 }
             }
     ?>
